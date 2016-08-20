d970d4d0cf55b2a75eb7e4159b563140cf39f5c6
avhimkov <avhimkov@mail.ru>
2016-08-20 16:14:23 +0500

    Initial commit

---
+++ src/test/java/org/adlsoft/AppTest.java
@@ -0,0 +1,15 @@
+package org.adlsoft;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+
+
+}
