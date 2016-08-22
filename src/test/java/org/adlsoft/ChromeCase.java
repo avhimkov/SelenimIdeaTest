b8581c1698efd4cf1e29a3ff6cc5022c98df75a6
avhimkov <avhimkov@mail.ru>
2016-08-22 17:38:29 +0500

    Clear code

--- src/test/java/org/adlsoft/ChromeCase.java
+++ src/test/java/org/adlsoft/ChromeCase.java
@@ -1,33 +1,26 @@
 package org.adlsoft;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class ChromeCase {
     private WebDriver driver;
     private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
 
     @Before
     public void logPass(){
-        baseUrl = "http://148.251.88.9:8080/";
+        baseUrl = "http://148.251.88.9:8080/mfc_hmao/admin/";
     }
 
     @Test
     public void testCase() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\doc\\FirefoxDriver\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\doc\\DriverBrowser\\chromedriver_win32\\chromedriver.exe");
         System.setProperty("webdriver.chrome.bin", "C:\\Users\\adl\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://148.251.88.9:8080/mfc_hmao/admin/");
@@ -36,25 +29,24 @@ public class ChromeCase {
         driver.findElement(By.name("login")).sendKeys("");
         driver.findElement(By.name("password")).sendKeys("");
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-        driver.get(baseUrl + "create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default");
-//        driver.findElement(By.id("plus10358393@SXFolder")).click();
-//        driver.findElement(By.linkText("Сотрудники")).click();
-//        Thread.sleep(5000);  // Let the user actually see something!
-//        WebElement searchBox = driver.findElement(By.name("q"));
-//        searchBox.sendKeys("ChromeDriver");
-//        searchBox.submit();
+        //Form create user
+        driver.get(baseUrl + "create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&7ca8a1c17f9e33d2ec6b498540532cfc");
+        driver.findElement(By.id("id_surname")).sendKeys("Авхимко");
+        driver.findElement(By.id("id_name")).sendKeys("Дмитрий");
+        driver.findElement(By.id("id_patronymic")).sendKeys("Леонидович");
+        driver.findElement(By.id("id_login")).sendKeys("AvhimkovDL");
+        driver.findElement(By.name("data(password)")).sendKeys("qwe123qwe");
+        driver.findElement(By.name("data(repassword)")).sendKeys("qwe123qwe");
+        new Select(driver.findElement(By.id("id_mfc"))).selectByVisibleText("Муниципальное казенное учреждение \"Многофункциональный центр предоставления государственных и муниципальных услуг г. Лянтор Сургутского района\"");
+        driver.findElement(By.id("formSubmit")).click();
+        //driver.findElement(By.id("title_operator")).click();
+        //driver.findElement(By.id("title_controller")).click();
+        //driver.findElement(By.id("title_docved")).click();
+        driver.findElement(By.id("title_esia")).click();
+
+
+
         Thread.sleep(5000);  // Let the user actually see something!
         driver.quit();
-
-
-//        System.setProperty("webdriver.firefox.marionette","D:\\doc\\FirefoxDriver\\geckodriver.exe");
-//        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-//
-//        driver = new FirefoxDriver();
-////        WebDriver driver = new ChromeDriver();
-//        driver.get("http://www.google.com");
-//        //driver.get("http://ya.ru/");
-//        driver.quit();
-//        driver.close();
     }
 }
--- src/test/java/org/adlsoft/FirefixCase.java
+++ src/test/java/org/adlsoft/FirefixCase.java
@@ -1,59 +1,35 @@
 package org.adlsoft;
 
-
-import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.support.ui.Select;
 
 public class FirefixCase {
     private WebDriver driver;
     private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-//    public String pass;
-//    public String logen;
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.firefox.marionette","D:\\doc\\FirefoxDriver\\geckodriver.exe");
-        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-        driver = new FirefoxDriver();
-        baseUrl = "http://148.251.88.9:8080/";
-        driver.get(baseUrl + "mfc_hmao/admin/");
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        baseUrl = "http://148.251.88.9:8080/mfc_hmao/admin/";
+
     }
 
     @Test
     public void testAppTest() throws Exception {
+        System.setProperty("webdriver.firefox.driver","D:\\doc\\DriverBrowser\\geckodriver.exe");
+        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        driver = new FirefoxDriver();
 
-        System.setProperty("webdriver.firefox.marionette","D:\\doc\\FirefoxDriver\\geckodriver.exe");
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите логин: ");
-        String login = in.nextLine();
-        System.out.print("Введите пароль: ");
-        String pass = in.nextLine();
-
-
-
-        driver.get(baseUrl + "/mfc_hmao/admin/");
-        //driver.findElement(By.name("login")).clear();
-        driver.findElement(By.name("login")).sendKeys("login");
-        driver.findElement(By.name("password")).sendKeys("pass");
+        driver.findElement(By.name("login")).sendKeys("");
+        driver.findElement(By.name("password")).sendKeys("");
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | nav | ]]
-        driver.findElement(By.id("plus10358393@SXFolder")).click();
-        driver.findElement(By.linkText("Сотрудники")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=toolbar | ]]
-        driver.findElement(By.cssSelector("td.menu_over > button.tm")).click();
-        // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _blank | 30000]]
-        driver.get(baseUrl + "/mfc_hmao/admin/create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&4ee81ab52eb8829b2b585fd95907bc67");
-
+        //Form create user
+        driver.get(baseUrl + "create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&7ca8a1c17f9e33d2ec6b498540532cfc");
         driver.findElement(By.id("id_surname")).sendKeys("Авхимко");
         driver.findElement(By.id("id_name")).sendKeys("Дмитрий");
         driver.findElement(By.id("id_patronymic")).sendKeys("Леонидович");
@@ -66,47 +42,10 @@ public class FirefixCase {
         //driver.findElement(By.id("title_controller")).click();
         //driver.findElement(By.id("title_docved")).click();
         driver.findElement(By.id("title_esia")).click();
-    }
 
-    @After
-    public void tearDown() throws Exception {
+        Thread.sleep(5000);  // Let the user actually see something!
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
 
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
