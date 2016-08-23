ed488695d9eee149d2882c6603c5bd4d11f3ad5a
avhimkov <avhimkov@mail.ru>
2016-08-23 11:01:49 +0500

    Peremen add

--- src/test/java/org/adlsoft/GolosovanieMfc.java
+++ src/test/java/org/adlsoft/GolosovanieMfc.java
@@ -1,5 +1,8 @@
 package org.adlsoft;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
@@ -16,12 +19,23 @@ public class GolosovanieMfc {
 
     private WebDriver driver;
     private String baseUrl;
-    public String numderDela;
-    public String codAutantif;
+    public String numderDela = "111111111";
+    public String codAutantif = "11111";
+
+    public String NumderDelta()throws FileNotFoundException {
+        Scanner in = new Scanner(new FileReader("file.txt"));
+        String numderDela = "";
+        while(in.hasNext())
+        {
+            numderDela = in.next();
+        }
+        return numderDela;
+    }
 
     @Before
-    public void logPass(){
-        baseUrl = "http://mfc.admhmao.ru/";
+    public void logPass()  {
+
+        //baseUrl = "http://mfc.admhmao.ru/";
     }
 
     @Test
@@ -29,11 +43,11 @@ public class GolosovanieMfc {
         System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
         System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get(baseUrl + "/mfctablet/quality.htm");
+        driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
         driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.name("fileNumber")).sendKeys("111111111");
+        driver.findElement(By.name("fileNumber")).sendKeys(numderDela);
         driver.findElement(By.name("authCode")).clear();
-        driver.findElement(By.name("authCode")).sendKeys("11111");
+        driver.findElement(By.name("authCode")).sendKeys(codAutantif);
         driver.findElement(By.xpath("//button[@type='button']")).click();
 
         Thread.sleep(5000);  // Let the user actually see something!
--- src/test/java/org/adlsoft/SistematicaCreateUsers.java
+++ src/test/java/org/adlsoft/SistematicaCreateUsers.java
@@ -9,15 +9,23 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.Select;
 
-public class ChromeCase {
+public class SistematicaCreateUsers {
     private WebDriver driver;
     private String baseUrl;
-    public String pass;
-    public String login;
+    public String login = "";
+    public String pass = "";
+
+    public String id_surname = "Авхимко";
+    public String id_name = "Дмитрий";
+    public String id_patronymic = "Леонидович";
+    public String id_login = "AvhimkovDL";
+    public String data_password = "qwe123qwe";
+    public String data_repassword = "qwe123qwe";
 
     @Before
     public void logPass(){
-        baseUrl = "http://148.251.88.9:8080/mfc_hmao/admin/";
+
+//        baseUrl = "http://148.251.88.9:8080/mfc_hmao/admin/";
     }
 
     @Test
@@ -28,23 +36,23 @@ public class ChromeCase {
         driver.get("http://148.251.88.9:8080/mfc_hmao/admin/");
 
 
-        driver.findElement(By.name("login")).sendKeys("");
-        driver.findElement(By.name("password")).sendKeys("");
+        driver.findElement(By.name("login")).sendKeys(login);
+        driver.findElement(By.name("password")).sendKeys(pass);
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
         //Form create user
-        driver.get(baseUrl + "create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&7ca8a1c17f9e33d2ec6b498540532cfc");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.id("id_surname")).sendKeys("Авхимко");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.id("id_name")).sendKeys("Дмитрий");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.id("id_patronymic")).sendKeys("Леонидович");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.id("id_login")).sendKeys("AvhimkovDL");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.name("data(password)")).sendKeys("qwe123qwe");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.name("data(repassword)")).sendKeys("qwe123qwe");
+        driver.get("http://148.251.88.9:8080/mfc_hmao/admin/create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&7ca8a1c17f9e33d2ec6b498540532cfc");
+        driver.findElement(By.id("id_surname")).clear();
+        driver.findElement(By.id("id_surname")).sendKeys(id_surname); //Авхимко
+        driver.findElement(By.id("id_name")).clear();
+        driver.findElement(By.id("id_name")).sendKeys(id_name); //Дмитрий
+        driver.findElement(By.id("id_patronymic")).clear();
+        driver.findElement(By.id("id_patronymic")).sendKeys(id_patronymic); //Леонидович
+        driver.findElement(By.id("id_login")).clear();
+        driver.findElement(By.id("id_login")).sendKeys(id_login); //AvhimkovDL
+        driver.findElement(By.name("data(password)")).clear();
+        driver.findElement(By.name("data(password)")).sendKeys(data_password); //qwe123qwe
+        driver.findElement(By.name("data(repassword)")).clear();
+        driver.findElement(By.name("data(repassword)")).sendKeys(data_repassword); //qwe123qwe
         new Select(driver.findElement(By.id("id_mfc"))).selectByVisibleText("Муниципальное казенное учреждение \"Многофункциональный центр предоставления государственных и муниципальных услуг г. Лянтор Сургутского района\"");
         driver.findElement(By.id("formSubmit")).click();
         //driver.findElement(By.id("title_operator")).click();
