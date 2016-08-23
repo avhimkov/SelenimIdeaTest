a081e0db3b446a736ac44594a0f2c6827949581c
avhimkov <avhimkov@mail.ru>
2016-08-23 23:09:45 +0500

    HashMap for number dela & code auth.

--- src/test/java/org/adlsoft/GolosovanieMfc.java
+++ src/test/java/org/adlsoft/GolosovanieMfc.java
@@ -1,94 +1,57 @@
 package org.adlsoft;
 
 import java.io.*;
-
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.io.Files;
 import org.junit.*;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.concurrent.TimeUnit;
 import java.lang.*;
-import com.csvreader.CsvReader;
-
-
-
-
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GolosovanieMfc{
-    //    private static final String FILENAME = "C:\\1.txt";
-    private WebDriver driver;
-    //    private String baseUrl;
-    public String numderDela = "21212121"; //111111111
-    public String codAutantif = "32323"; //11111
-
-/* ================================================================ */
-    private String login() {
-    String csvFile = "D:\\Golos.csv";
-    String line = "";
-    final String cvsSplitBy = ";";
-    BufferedReader br = null;
-
-        try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-                String[] code = line.split(cvsSplitBy);
-
-                codAutantif = code[4];
-            }
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return codAutantif;
-    }
-
-
-/* ================================================================ */
 
     @Before
     public void logPass() {
-
         //baseUrl = "http://mfc.admhmao.ru/";
     }
 
     @Test
-    public void testCase() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
-        driver.findElement(By.name("fileNumber")).clear();
-        driver.findElement(By.name("fileNumber")).sendKeys(numderDela);
-        driver.findElement(By.name("authCode")).clear();
-        driver.findElement(By.name("authCode")).sendKeys(codAutantif);
-        driver.findElement(By.xpath("//button[@type='button']")).click();
+    public void testCase() throws InterruptedException, IOException {
+        WebDriver driver;
+//        String numderDela; //111111111
+//        String codAutantif; //11111
+        String csvFile = "D:\\Golos.csv";
+        String line = null;
+        final String cvsSplitBy = ";";
+        BufferedReader br = null;
 
-        Thread.sleep(5000);  // Let the user actually see something!
-        driver.quit();
+        br = new BufferedReader(new FileReader(csvFile));
+        HashMap<String,String> map = new HashMap<String, String>();
+        while((line=br.readLine())!=null) {
+            String str[] = line.split(";");
+            for (Map.Entry entry : map.entrySet()) {
+                String key = (String) entry.getKey();
+                String value = (String) entry.getValue();
+
+                // use comma as separator
+                String[] code = line.split(cvsSplitBy);
+                System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+                driver = new ChromeDriver();
+                driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
+                driver.findElement(By.name("fileNumber")).clear();
+                driver.findElement(By.name("fileNumber")).sendKeys(key);
+                driver.findElement(By.name("authCode")).clear();
+                driver.findElement(By.name("authCode")).sendKeys(value);
+                driver.findElement(By.xpath("//button[@type='button']")).click();
+
+                Thread.sleep(5000);  // Let the user actually see something!
+                driver.quit();
+
+            }
+
+        }
     }
 
 }
