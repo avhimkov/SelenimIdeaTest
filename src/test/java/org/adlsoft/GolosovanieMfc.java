c6f020a068705a3a2d4a7808f4b0c6bd6cee67e8
avhimkov <avhimkov@mail.ru>
2016-08-24 15:16:43 +0500

    GoHome

--- src/test/java/org/adlsoft/GolosovanieMfc.java
+++ src/test/java/org/adlsoft/GolosovanieMfc.java
@@ -1,15 +1,17 @@
 package org.adlsoft;
 
 import java.io.*;
+
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+
 import java.lang.*;
 import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GolosovanieMfc{
+public class GolosovanieMfc {
 
     @Before
     public void logPass() {
@@ -27,31 +29,27 @@ public class GolosovanieMfc{
         BufferedReader br = null;
 
         br = new BufferedReader(new FileReader(csvFile));
-        HashMap<String,String> map = new HashMap<String, String>();
-        while((line=br.readLine())!=null) {
-            String str[] = line.split(";");
-            for (Map.Entry entry : map.entrySet()) {
-                String key = (String) entry.getKey();
-                String value = (String) entry.getValue();
+        // use comma as separator
+        String[] code = line.split(cvsSplitBy);
+        Map<String, String> map = new HashMap<String, String>();
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            String key = item.getKey();
+            String value = item.getValue();
+            System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+            driver = new ChromeDriver();
+            driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
+            driver.findElement(By.name("fileNumber")).clear();
+            driver.findElement(By.name("fileNumber")).sendKeys(key);
+            driver.findElement(By.name("authCode")).clear();
+            driver.findElement(By.name("authCode")).sendKeys(value);
+//                driver.findElement(By.xpath("//button[@type='button']")).click();
 
-                // use comma as separator
-                String[] code = line.split(cvsSplitBy);
-                System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
-                System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
-                driver = new ChromeDriver();
-                driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
-                driver.findElement(By.name("fileNumber")).clear();
-                driver.findElement(By.name("fileNumber")).sendKeys(key);
-                driver.findElement(By.name("authCode")).clear();
-                driver.findElement(By.name("authCode")).sendKeys(value);
-                driver.findElement(By.xpath("//button[@type='button']")).click();
-
-                Thread.sleep(5000);  // Let the user actually see something!
-                driver.quit();
-
-            }
+            Thread.sleep(5000);  // Let the user actually see something!
+            driver.quit();
 
         }
+
     }
 
 }
