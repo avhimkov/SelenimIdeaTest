b0fb1e5ae890d9e0768516545adf70186a0a4ac6
avhimkov <avhimkov@mail.ru>
2016-08-25 22:12:39 +0500

    loope work

--- src/test/java/org/adlsoft/GolosovanieMfc.java
+++ src/test/java/org/adlsoft/GolosovanieMfc.java
@@ -17,41 +17,40 @@ public class GolosovanieMfc{
 
 
     @Before
-    public void logPass() {
-        //baseUrl = "http://mfc.admhmao.ru/";
+    public void logPass(){
+
     }
 
     @Test
-    public void testCase() throws InterruptedException, IOException {
+    public void testCase() throws IOException, InterruptedException {
          WebDriver driver;
-         String csvFile = "D:\\Golos.csv";
-         String line = "";
-         StringTokenizer tokens = null;
-         String cvsSplitBy = ";";
-         BufferedReader reader = null;
 
-        reader = new BufferedReader(new FileReader(csvFile));
-        Map<String, String> map = new HashMap<>();
+
+        String csvFile = "D:\\Golos.csv";
+        String line = "";
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
         while((line=reader.readLine())!=null) {
             String str[] = line.split(";");
             if (str.length > 1) {
-                map.put(str[0].trim(), str[1]);
-            }
-
                 // use comma as separator
-                String[] code = line.split(cvsSplitBy);
                 System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver_win32\\chromedriver.exe");
                 System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
                 driver = new ChromeDriver();
                 driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
                 driver.findElement(By.name("fileNumber")).clear();
-                driver.findElement(By.name("fileNumber")).sendKeys(map.get(1));
+                driver.findElement(By.name("fileNumber")).sendKeys(str[1]);
                 driver.findElement(By.name("authCode")).clear();
-                driver.findElement(By.name("authCode")).sendKeys(map.get(4));
+                driver.findElement(By.name("authCode")).sendKeys(str[4]);
                 driver.findElement(By.xpath("//button[@type='button']")).click();
 
                 Thread.sleep(5000);  // Let the user actually see something!
                 driver.quit();
+            }
+
+
+
+
+
 
             }
 
