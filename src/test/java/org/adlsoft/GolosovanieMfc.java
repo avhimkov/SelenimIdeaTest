7c0a3b38e6d8515b5dabb4ae4ad2762098630df2
avhimkov <avhimkov@mail.ru>
2016-08-25 22:24:48 +0500

    Create users Sistematika

--- src/test/java/org/adlsoft/GolosovanieMfc.java
+++ src/test/java/org/adlsoft/GolosovanieMfc.java
@@ -9,33 +9,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class GolosovanieMfc{
-
-    //        String numderDela; //111111111
-    //        String codAutantif; //11111
-
+public class GolosovanieMfc {
 
 
     @Before
-    public void logPass(){
+    public void logPass() {
 
     }
 
     @Test
     public void testCase() throws IOException, InterruptedException {
-         WebDriver driver;
-
-
+        WebDriver driver = new ChromeDriver();
         String csvFile = "D:\\Golos.csv";
         String line = "";
         BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-        while((line=reader.readLine())!=null) {
+        while ((line = reader.readLine()) != null) {
             String str[] = line.split(";");
             if (str.length > 1) {
                 // use comma as separator
                 System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver_win32\\chromedriver.exe");
                 System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
-                driver = new ChromeDriver();
+
                 driver.get("http://mfc.admhmao.ru/mfctablet/quality.htm");
                 driver.findElement(By.name("fileNumber")).clear();
                 driver.findElement(By.name("fileNumber")).sendKeys(str[1]);
@@ -47,13 +41,8 @@ public class GolosovanieMfc{
                 driver.quit();
             }
 
-
-
-
-
-
-            }
-
         }
+
     }
+}
 
--- src/test/java/org/adlsoft/SistematicaCreateUsers.java
+++ src/test/java/org/adlsoft/SistematicaCreateUsers.java
@@ -1,5 +1,8 @@
 package org.adlsoft;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
@@ -10,35 +13,48 @@ import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.Select;
 
 public class SistematicaCreateUsers {
-    private WebDriver driver;
-    private String baseUrl;
-    public String login = "AvhimkovDL";
-    public String pass = "";
-
-    public String id_surname = "Авхимко";
-    public String id_name = "Дмитрий";
-    public String id_patronymic = "Леонидович";
-    public String id_login = "AvhimkovDL";
-    public String data_password = "qwe123qwe";
-    public String data_repassword = "qwe123qwe";
+//    private WebDriver driver;
+//    private String baseUrl;
+//    public String login = "AvhimkovDL";
+//    public String pass = "";
+//
+//    public String id_surname = "Авхимко";
+//    public String id_name = "Дмитрий";
+//    public String id_patronymic = "Леонидович";
+//    public String id_login = "AvhimkovDL";
+//    public String data_password = "qwe123qwe";
+//    public String data_repassword = "qwe123qwe";
 
     @Before
-    public void logPass(){
+    public void logPass() {
 
 //        baseUrl = "http://148.251.88.9:8080/mfc_hmao/admin/";
     }
 
     @Test
-    public void testCase() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+    public void testCase() throws InterruptedException, IOException {
         WebDriver driver = new ChromeDriver();
+
         driver.get("http://148.251.88.9:8080/mfc_hmao/admin/");
+        String csvFile = "D:\\loginOperator.csv";
+        String line = "";
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+        while ((line = reader.readLine()) != null) {
+            String str[] = line.split(";");
+            if (str.length > 1) {
+                System.setProperty("webdriver.chrome.driver", "D:\\DriverBrowser\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.bin", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+                driver.findElement(By.name("login")).sendKeys(str[0]);
+                driver.findElement(By.name("password")).sendKeys(str[1]);
+                driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+//                Thread.sleep(5000);  // Let the user actually see something!
+                driver.quit();
+            }
+        }
+    }
 
-
-        driver.findElement(By.name("login")).sendKeys(login);
-        driver.findElement(By.name("password")).sendKeys(pass);
-        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+    public void CreateUser() {
+        /*
         //Form create user
         driver.get("http://148.251.88.9:8080/mfc_hmao/admin/create.htm?id=11478361@SXFolder&cls=mfcUser&ra=members&ds=default&link=11478361@SXFolder&7ca8a1c17f9e33d2ec6b498540532cfc");
         driver.findElement(By.id("id_surname")).clear();
@@ -59,10 +75,6 @@ public class SistematicaCreateUsers {
         //driver.findElement(By.id("title_controller")).click();
         //driver.findElement(By.id("title_docved")).click();
         driver.findElement(By.id("title_esia")).click();
-
-
-
-        Thread.sleep(5000);  // Let the user actually see something!
-        driver.quit();
+        */
     }
 }
