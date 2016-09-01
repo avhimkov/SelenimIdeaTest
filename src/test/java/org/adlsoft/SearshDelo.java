7cdf359e04963ad8dc983fcd1b4d27af19bc65bf
avhimkov <avhimkov@mail.ru>
2016-09-01 12:20:59 +0500

    Del Login class.

--- src/test/java/org/adlsoft/SearshDelo.java
+++ src/test/java/org/adlsoft/SearshDelo.java
@@ -2,12 +2,10 @@ package org.adlsoft;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
-import org.adlsoft.Login;
 
 
 public class SearshDelo {
