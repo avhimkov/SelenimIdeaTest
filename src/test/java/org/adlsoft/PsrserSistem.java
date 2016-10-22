339d37f8bfdc89c6da6a34422ed1d981d5886dcb
avhimkov <avhimkov@mail.ru>
2016-10-22 23:51:03 +0500

    tag a name data(folderNumber)

--- src/test/java/org/adlsoft/PsrserSistem.java
+++ src/test/java/org/adlsoft/PsrserSistem.java
@@ -19,31 +19,30 @@ public class PsrserSistem {
     public static void main(String[] args) throws IOException {
         File input = new File("src/html/MFC12.html");
         Document doc = Jsoup.parse(input,"UTF-8");
-        Elements tableRows=doc.getElementsByTag("tr");
-        for(Element row : tableRows)
-        {
-            Elements tableHeaders=row.getElementsByTag("th");
+        Elements tableRows = doc.getElementsByTag("tr");
+//        Elements links = doc.getElementsByTag("a");
+
+        for(Element row : tableRows) {
+            Elements tableHeaders = row.getElementsByTag("a").attr("name", "data(folderNumber)");
+            //get text content of each cell element
             for (Element element : tableHeaders) {
-                //get text content of each cell element
                 String elementText = element.text();
                 //print out the content
-                System.out.print(elementText +" ");
+                System.out.print(elementText + " ");
             }
         }
         //get each row to content without header
-        for(Element row : tableRows)
-        {
-            Elements tableCells=row.getElementsByTag("td");
-            //iterate over each element
-            for (Element element : tableCells) {
-                //get text content of each cell element
-                String elementText = element.text();
-                //print out the content
-                System.out.print(elementText +" ");
-            }
-            System.out.println();
-        }
+//        for(Element row : tableRows)
+//        {
+//            Elements tableCells=row.getElementsByTag("td");
+//            //iterate over each element
+//            for (Element element : tableCells) {
+//                //get text content of each cell element
+//                String elementText = element.text();
+//                //print out the content
+//                System.out.print(elementText +" ");
+//            }
+//            System.out.println();
+//        }
     }
-
-
 }
