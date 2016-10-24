175b4fd74e1cf18a7a469e1be0b1303d73c48c0d
avhimkov <avhimkov@mail.ru>
2016-10-24 19:44:12 +0500

    Fix find string MFC

--- src/test/java/org/adlsoft/PsrserSistem.java
+++ src/test/java/org/adlsoft/PsrserSistem.java
@@ -21,7 +21,22 @@ public class PsrserSistem {
 
 //      "../MFC12.html");
         Document doc = Jsoup.parse(new File("src/html/MFC12.html"), "UTF-8");
-        Elements tableRows = doc.getElementsByTag("td");
+        Elements tableRows = doc.getElementsByTag("tr");
+
+        try {
+            for (Element row : tableRows) {
+                Elements tableCells = row.getElementsByTag("tr");
+                String elementText = tableCells.text();
+                String[] arrstring = elementText.split(" ");
+                int result = elementText.indexOf("MFC");
+                String text = elementText.substring(result, result+14);
+                System.out.println(text);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+//            e.printStackTrace();
+//            System.out.println("Error");
+        }
+
 //        for(Element row : tableRows) {
 //            Elements tableHeaders = row.getElementsByTag("tr");
 //            for (Element element : tableHeaders) {
@@ -29,33 +44,5 @@ public class PsrserSistem {
 //                System.out.print(elementText + " ");
 //            }
 //        }
-        try {
-            for (Element row : tableRows) {
-                Elements tableCells = row.getElementsByTag("td");
-                String elementText = tableCells.text();
-//                boolean result = elementText.contains("MFC");
-                String[] text = elementText.split(" ");
-                if (elementText.matches("MFC")){
-                    String result = text[5];
-                    System.out.println(result);
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-//            e.printStackTrace();
-//            System.out.println("Error");
-        }
     }
-
-//    static void findString(String put, String findeSring) throws IOException  {
-//        List<String> list = new ArrayList<>();
-//        try (Stream<String> stream = Files.lines(Paths.get(put), Charset.forName("windows-1251"))) {
-//            list = stream
-//                    .filter(line -> line.contains())
-//                    .filter(line -> line.contains())
-//                    .collect(Collectors.toList());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        list.forEach(System.out::println);
-//    }
 }
